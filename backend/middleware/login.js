aa938dc1a4884d3cccacc15f128b70eb5163323d
Erick <santoserick9@gmail.com>
2022-03-13 17:09:37 -0300

    chore: remove console.log

--- backend/middleware/login.js
+++ backend/middleware/login.js
@@ -4,9 +4,9 @@ exports.required = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decode = jwt.verify(token, process.env.JWT_KEY);
-    console.log(decode, "decode");
-    console.log("aqui");
+
     req.user = decode;
+
     next();
   } catch (error) {
     return res.status(401).send({ message: "Authentication Fails" });
