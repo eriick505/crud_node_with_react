eb64f7d54a287b53294060e9fec060f42bacbc3f
Erick <santoserick9@gmail.com>
2022-03-18 13:07:55 -0300

    feat: add type for model

--- backend-mongodb/src/models/User.ts
+++ backend-mongodb/src/models/User.ts
@@ -1,14 +1,14 @@
 import { Schema, model } from "mongoose";
 
-import type { IUser } from "src/types/user";
+import type { IUserRequest } from "@type/user";
 
-const userSchema = new Schema<IUser>({
+const userSchema = new Schema<IUserRequest>({
   name: { type: String, required: true },
   phone: { type: Number, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-const User = model<IUser>("User", userSchema);
+const User = model<IUserRequest>("User", userSchema);
 
 export default User;
