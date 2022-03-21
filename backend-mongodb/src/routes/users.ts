7406a56cae530b05846cc317246eac8bbadfcb1a
Erick <santoserick9@gmail.com>
2022-03-20 22:54:32 -0300

    feat: add routes to handle user

--- backend-mongodb/src/routes/users.ts
+++ backend-mongodb/src/routes/users.ts
@@ -1,25 +1,10 @@
 import { Router } from "express";
-import { getUsersData, signUp, login } from "@controllers/users";
-
-// import createUserData from "../controllers/users/createUserData.js";
-// import editUserById from "../controllers/users/editUserById.js";
-// import getUserByToken from "../controllers/users/getUserByToken.js";
-
-// import getUsersData from "../controllers/users/getUsersData.js";
-
-// import signup from "../controllers/users/signUp";
-// import login from "../controllers/users/login.js";
+import { signUp, login, getUserByToken } from "@controllers/users";
 
 const userRouter = Router();
 
-userRouter.get("/all-users", getUsersData);
-// userRouter.get("/:id", getUserById); NÃO EXISTE MAIS
-
-// userRouter.put("/:id", editUserById);
-// userRouter.post("/create-user", createUserData);
-
 userRouter.post("/signup", signUp);
 userRouter.post("/login", login);
-// userRouter.post("/get-user", getUserByToken);
+userRouter.post("/get-user", getUserByToken);
 
 export default userRouter;
