3d698971b9f7bfd616cf895e45402aac764da4b2
Erick <santoserick9@gmail.com>
2022-03-20 22:53:50 -0300

    chore: change type of Schema/Modal

--- backend-mongodb/src/models/User.ts
+++ backend-mongodb/src/models/User.ts
@@ -1,14 +1,14 @@
 import { Schema, model } from "mongoose";
 
-import type { IUserRequest } from "@type/user";
+import type { IUserSignupRequest } from "@type/user";
 
-const userSchema = new Schema<IUserRequest>({
+const userSchema = new Schema<IUserSignupRequest>({
   name: { type: String, required: true },
   phone: { type: Number, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-const User = model<IUserRequest>("User", userSchema);
+const User = model<IUserSignupRequest>("User", userSchema);
 
 export default User;
